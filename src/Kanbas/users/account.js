2fce492949dfca8127e2d5209643cc07ca853f6f
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2023-12-03 16:33:17 -0500

    Update account.js

--- src/Kanbas/users/account.js
+++ src/Kanbas/users/account.js
@@ -68,7 +68,7 @@ function Account() {
               </select>
             </div>
             <div className="d-flex justify-content-center">
-              <button onClick={save} classname="btn btn-primary">
+              <button onClick={save} className="btn btn-primary">
                 Save
               </button>
             </div>
