c70642a9e66e214bbe60b8012ee88e52b8c52878
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2023-12-03 17:14:58 -0500

    Update client.js

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -17,6 +17,11 @@ export const signin = async (credentials) => {
   return response.data;
 };
 
+export const signout = async () => {
+  const response = await axios.post(`${USERS_API}/signout`);
+  return response.data;
+};
+
 export const account = async () => {
     const response = await request.post(`${USERS_API}/account`);
     return response.data;
